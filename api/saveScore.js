9df83d0ea6ee964a1b0da8a5a5ca3386bdfc8cbd
Matthew <mchen146@ucsc.edu>
2022-10-16 01:46:26 -0700

    try to save to specific database

--- api/saveScore.js
+++ api/saveScore.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
     const { body } = req;
     // return res.send(`TEST TESTSETSET Hello ${body.name}, you just parsed the request body!`);
 
-    await mongoose.connect(`mongodb+srv://mattowskinator:${process.env.MONGODB_PASS}@cluster0.t3jerdy.mongodb.net/?retryWrites=true&w=majority`);
+    await mongoose.connect(`mongodb+srv://mattowskinator:${process.env.MONGODB_PASS}@cluster0.t3jerdy.mongodb.net/ghf?retryWrites=true&w=majority`);
 
     const userSchema = new mongoose.Schema({
         name: String,
